d5efcaa337e7500ae72e0ba73191b53fae3388d2
wisdomwest <harmonykoome@gmail.com>
2023-05-22 10:10:10 -0700

    revert

---
+++ my-shell.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int shell(void)
+{
+	char *args;
+	char **tokens;
+
+	while (1)
+	{
+		args = read_input();
+		tokens = process_args(args);
+		execute(tokens);
+	}
+	return (0);
+}
